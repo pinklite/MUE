c1471bfd99636d0bb4ad0f975ba0bbfc3a12c099
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-07-15 14:25:57 -0600

    f

--- src/qt/proposallist.cpp
+++ src/qt/proposallist.cpp
@@ -222,24 +222,14 @@ ProposalList::ProposalList(   QWidget *parent) :
     proposalList->sortByColumn(ProposalTableModel::StartDate, Qt::DescendingOrder);
     proposalList->verticalHeader()->hide();
 
-    int PROPOSAL_COLUMN_WIDTH = 200;
-    int START_DATE_COLUMN_WIDTH = 70;
-    int END_DATE_COLUMN_WIDTH = 70;
-    int YES_VOTES_COLUMN_WIDTH = 60;
-    int NO_VOTES_COLUMN_WIDTH = 60;
-    int ABSTAIN_COLUMN_WIDTH = 60;
-    int AMOUNT_COLUMN_WIDTH = 100;
-    int PERCENTAGE_COLUMN_WIDTH = 80;
-    int MINIMUM_COLUMN_WIDTH = 23;	
-	
-    proposalList->setColumnWidth(0, PROPOSAL_COLUMN_WIDTH);
-    proposalList->setColumnWidth(1, AMOUNT_COLUMN_WIDTH);
-    proposalList->setColumnWidth(2, START_DATE_COLUMN_WIDTH);
-    proposalList->setColumnWidth(3, END_DATE_COLUMN_WIDTH);
-    proposalList->setColumnWidth(4, YES_VOTES_COLUMN_WIDTH);
-    proposalList->setColumnWidth(5, NO_VOTES_COLUMN_WIDTH);
-    proposalList->setColumnWidth(6, ABSTAIN_COLUMN_WIDTH);
-    proposalList->setColumnWidth(7, PERCENTAGE_COLUMN_WIDTH);
+    proposalList->setColumnWidth(ProposalTableModel::Proposal, PROPOSAL_COLUMN_WIDTH);
+    proposalList->setColumnWidth(ProposalTableModel::StartDate, START_DATE_COLUMN_WIDTH);
+    proposalList->setColumnWidth(ProposalTableModel::EndDate, END_DATE_COLUMN_WIDTH);
+    proposalList->setColumnWidth(ProposalTableModel::YesVotes, YES_VOTES_COLUMN_WIDTH);
+    proposalList->setColumnWidth(ProposalTableModel::NoVotes, NO_VOTES_COLUMN_WIDTH);
+    proposalList->setColumnWidth(ProposalTableModel::AbstainVotes, ABSTAIN_COLUMN_WIDTH);
+    proposalList->setColumnWidth(ProposalTableModel::Amount, AMOUNT_COLUMN_WIDTH);
+    proposalList->setColumnWidth(ProposalTableModel::Percentage, PERCENTAGE_COLUMN_WIDTH);
 
     connect(proposalList->selectionModel(), SIGNAL(selectionChanged(QItemSelection, QItemSelection)), this, SLOT(computeSum()));
 	
