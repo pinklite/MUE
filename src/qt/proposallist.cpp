192e591721b4e05b79a9f01f7c758b7b4361dc8c
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-07-15 17:17:50 -0600

    f

--- src/qt/proposallist.cpp
+++ src/qt/proposallist.cpp
@@ -233,7 +233,7 @@ ProposalList::ProposalList(   QWidget *parent) :
 
     connect(proposalList->selectionModel(), SIGNAL(selectionChanged(QItemSelection, QItemSelection)), this, SLOT(computeSum()));
 	
-    columnResizingFixer = new GUIUtil::TableViewLastColumnResizingFixer(proposalList, PROPOSAL_COLUMN_WIDTH, 0);
+    columnResizingFixer = new GUIUtil::TableViewLastColumnResizingFixer(proposalList, PROPOSAL_COLUMN_WIDTH, MINIMUM_COLUMN_WIDTH );
         
 
 
