2c71cf428afe28b9b654a8a480bfaa591511210e
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-07-15 12:25:33 -0600

    f

--- src/qt/proposaltablemodel.cpp
+++ src/qt/proposaltablemodel.cpp
@@ -115,7 +115,7 @@ void ProposalTableModel::refreshProposals() {
 		
         if(mnCount > 0) percentage = round(pbudgetProposal->GetYeas() * 100 / mnCount);
         int amount = 0;
-		amount = ValueFromAmount(pbudgetProposal->GetAmount() * pbudgetProposal->GetTotalPaymentCount()).toInt();
+		int amount = ValueFromAmount(pbudgetProposal->GetAmount() * pbudgetProposal->GetTotalPaymentCount()).toInt();
 		
         proposalRecords.append(new ProposalRecord(
                         QString::fromStdString(pbudgetProposal->GetHash().ToString()),
