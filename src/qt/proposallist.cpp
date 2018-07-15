a45b40e7d9554223d6a0f969285b7cd7a553c3f5
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-07-15 12:50:00 -0600

    f

--- src/qt/proposallist.cpp
+++ src/qt/proposallist.cpp
@@ -64,7 +64,7 @@ ProposalList::ProposalList(   QWidget *parent) :
 #if QT_VERSION >= 0x040700
     amountWidget->setPlaceholderText(tr("Min amount"));
 #endif
-    amountWidget->setValidator(new QDoubleValidator(0, 1e20, 8, this));
+    amountWidget->setValidator(new QDoubleValidator(0, 1e20, 0, this));
     amountWidget->setObjectName("amountWidget");
     hlayout->addWidget(amountWidget);
 
