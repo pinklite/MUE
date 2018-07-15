050898b0cc0ec6d0d3d7f7521f2aa021a0444427
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-07-15 14:15:21 -0600

    f

--- src/qt/proposallist.cpp
+++ src/qt/proposallist.cpp
@@ -222,14 +222,14 @@ ProposalList::ProposalList(   QWidget *parent) :
     proposalList->sortByColumn(ProposalTableModel::StartDate, Qt::DescendingOrder);
     proposalList->verticalHeader()->hide();
 
-    proposalList->setColumnWidth(ProposalTableModel::Proposal, PROPOSAL_COLUMN_WIDTH);
-    proposalList->setColumnWidth(ProposalTableModel::StartDate, START_DATE_COLUMN_WIDTH);
-    proposalList->setColumnWidth(ProposalTableModel::EndDate, END_DATE_COLUMN_WIDTH);
-    proposalList->setColumnWidth(ProposalTableModel::YesVotes, YES_VOTES_COLUMN_WIDTH);
-    proposalList->setColumnWidth(ProposalTableModel::NoVotes, NO_VOTES_COLUMN_WIDTH);
-    proposalList->setColumnWidth(ProposalTableModel::AbstainVotes, ABSTAIN_COLUMN_WIDTH);
-    proposalList->setColumnWidth(ProposalTableModel::Amount, AMOUNT_COLUMN_WIDTH);
-    proposalList->setColumnWidth(ProposalTableModel::Percentage, PERCENTAGE_COLUMN_WIDTH);
+    proposalList->setColumnWidth(0, PROPOSAL_COLUMN_WIDTH);
+    proposalList->setColumnWidth(1, START_DATE_COLUMN_WIDTH);
+    proposalList->setColumnWidth(2, END_DATE_COLUMN_WIDTH);
+    proposalList->setColumnWidth(3, YES_VOTES_COLUMN_WIDTH);
+    proposalList->setColumnWidth(4, NO_VOTES_COLUMN_WIDTH);
+    proposalList->setColumnWidth(5, ABSTAIN_COLUMN_WIDTH);
+    proposalList->setColumnWidth(6, AMOUNT_COLUMN_WIDTH);
+    proposalList->setColumnWidth(7, PERCENTAGE_COLUMN_WIDTH);
 
     connect(proposalList->selectionModel(), SIGNAL(selectionChanged(QItemSelection, QItemSelection)), this, SLOT(computeSum()));
 	
