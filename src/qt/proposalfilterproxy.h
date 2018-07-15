f0c9c7a33b5610bb34b023cc72babd3f28d679f4
mrmetech <31669092+mrmetech@users.noreply.github.com>
2018-07-14 23:13:16 -0600

    f

--- src/qt/proposalfilterproxy.h
+++ src/qt/proposalfilterproxy.h
@@ -18,12 +18,12 @@ public:
     explicit ProposalFilterProxy(QObject *parent = 0);
 
 
-    static const QDateTime MIN_DATE;
+    //static const QDateTime MIN_DATE;
 
-    static const QDateTime MAX_DATE;
+    //static const QDateTime MAX_DATE;
 
-    void setProposalStart(const QDateTime &date);
-    void setProposalEnd(const QDateTime &date);
+    void setProposalStart(const CAmount& minimum);
+    void setProposalEnd(const CAmount& minimum);
     void setProposal(const QString &proposal);
     
     void setMinAmount(const CAmount& minimum);
@@ -38,8 +38,8 @@ protected:
     bool filterAcceptsRow(int source_row, const QModelIndex & source_parent) const;
 
 private:
-    QDateTime startDate;
-    QDateTime endDate;
+    CAmount startDate;
+    CAmount endDate;
     QString proposalName;
     CAmount minAmount;
     CAmount minPercentage;
