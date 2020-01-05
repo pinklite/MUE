480826c03fe3044f3e2a5cf89c83a9dd5a3eb1a6
tohsnoom <moonshot@phore.io>
2020-01-05 01:55:45 -0600

    Updated checkpoints

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -71,7 +71,8 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
     (600000, uint256("f62f6fbb0b1b901871de080a05cb6f64858028102ae58a20398421174db77f13"))
     (700000, uint256("87e7c895c50017f145b4fe50d3f31333523cad0389f8d3fb52958ef977ac7bcb"))
     (784600, uint256("aa9674333efbf557f3f15f852adde3880731dfb326ff73fe30df919d698e5d22"))
-    (791800, uint256("041973e61963048218f91c72fad5eacd6056c455bcef6d633f607f6edc6c3989"));
+    (791800, uint256("041973e61963048218f91c72fad5eacd6056c455bcef6d633f607f6edc6c3989"))
+    (849300, uint256("b5bc1303a62aacdd96aabc86342e9c4e1ce8c11548de6b2b15c1ec01fdfd6ec9"));
 
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
